60052e92ee29f6c368b12e9763b8274ab3b1eae8
Bread <jmagri1@uncc.edu>
2024-11-03 12:30:30 -0500

    created candidate interface and established partial candidate search skeleton

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,12 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+// name, username, location, avatar, email, html_url, and company
+export default interface Candidate {
+    readonly name: string | null;
+    readonly username: string | null;
+    readonly location: string | null;
+    readonly avatar: string | null;
+    readonly email: string | null;
+    readonly html_url: string | null;
+    readonly company: string | null;
+  }
+  
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,7 +1,31 @@
-import { useState, useEffect } from 'react';
+import { type FormEvent, useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import type Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
+  const [searchInput, setSearchInput] = useState<string>('');
+
+  const [currentCandidate, setCurrentCandidate] = useState<Candidate>({
+    name: '',
+    username: '',
+    location: '',
+    avatar: '',
+    email: '',
+    html_url: '',
+    company: '',
+  });
+
+  const searchGithubCandidates = () => {
+    
+  }
+
+  const searchForCandidateByUsername = async (event: FormEvent, username: string) => {
+    event.preventDefault();
+    const data: Candidate = await searchGithubUser(username);
+
+    setCurrentCandidate(data);
+  };
+
   return <h1>CandidateSearch</h1>;
 };
 
